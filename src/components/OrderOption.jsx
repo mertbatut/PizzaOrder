f0a547589ab721ede0f0e92ff9334513dd090b56
mertbatut <mertbatut@gmail.com>
2024-05-29 20:00:17 +0300

    Order Bölümü Tamamlandı

--- src/components/OrderOption.jsx
+++ src/components/OrderOption.jsx
@@ -1,26 +1,6 @@
 import React from 'react';
+import CheckBox from './CheckBox'; // Adjust the import path if necessary
 import '../index.css';
-
-const CheckBox = ({ label, handleCheck }) => {
-  const [checked, setChecked] = React.useState(false);
-
-  const onChange = () => {
-    setChecked(!checked);
-    handleCheck(!checked);
-  };
-
-  return (
-    <div className='flex items-center gap-2'>
-      <input
-        className={`w-[45px] h-[45px] rounded-md ${checked ? 'bg-[#FDC913]' : 'bg-[#FAF7F2]'}`}
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-      />
-      <label onClick={onChange} className="cursor-pointer">{label}</label>
-    </div>
-  );
-};
 
 const OrderOption = ({ handleCheck }) => {
   return (
