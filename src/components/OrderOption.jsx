524abf3b98620ac5010acd1f831117b35e65d5df
mertbatut <mertbatut@gmail.com>
2024-05-29 20:09:54 +0300

    Order Complated 1

--- src/components/OrderOption.jsx
+++ src/components/OrderOption.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CheckBox from './CheckBox'; // Adjust the import path if necessary
+import CheckBox from './CheckBox'; 
 import '../index.css';
 
 const OrderOption = ({ handleCheck }) => {
