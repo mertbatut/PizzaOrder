b59d21b3d546b8a2104ed2f2e893cea5020bd40b
mertbatut <mertbatut@gmail.com>
2024-05-31 15:11:16 +0300

    Home Page Başlangıç

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useRef } from 'react';
-import PizzaProductSection from './Pages/PizzaProductSection';
-
+// import PizzaProductSection from './Pages/PizzaProductSection';
+import Home from './Pages/Home';
 function App() {
   
 
   return (
     <div>
-     <PizzaProductSection/>
+     {/* <PizzaProductSection/> */}
+     <Home/>
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import OrderButton from '../components/OrderButton'
-import Footer from '../components/Footer'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <OrderButton/>
-        <Footer/>
-      </div>
+      <>
+      
+        <div className='MainImg '>
+          <div className='bg-[#CE2829]'>
+          <div><img className='absolute -z-10' src="https://s3-alpha-sig.figma.com/img/a770/35a9/df7ba412f5e1582f8e31e2e22182bd40?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n~gRjKTTvZQQzxsi~vWVwETQUcURvlHaCmwq6p-~ht4zMm89HJ5P3JKRGWX-BnrZRKXBFh1TZ28VPLX9H6TPM-1Czr-zGjDFno5NT~Ntv835xHNHJVnvTSJLAiguuWxrFNjy2nS0~iA~YsUbT3I5CFhnxBY-JqFAaYp7HoySdolGlGM3fm6T2nyAY~wSsYy8D16t4urNw7wizD7vk78k8D7227iEXfG28olbSInvU0nR8w2C8rfe9WhLBJ91bpqbakoDebsxI1-5u26XZxthGSebzgkaEa8Iyf59o0iqlQzOJs56HJlZ3kynt2vKNT43YyZpRFAf9cswmP4FQyf-NA__" alt="" />
+          </div>
+          <img className='absolute z-10' src="https://s3-alpha-sig.figma.com/img/2b78/c96a/4a4acb450e3bbb3750c05c017efa1f61?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NZhvKB0lpoLGgAB66kypq0PQzeT6wHC5VXopuIb9433NcTnZhhaSKofIjayKogdW0DKJvYHiLAXgEp0fnGD6Ok5nsV01P7N2azMdAgStqhFAMkKtjBwbPE49CbCh~qI8C1y9HUanDua-f7dCLaJjjr8f2ZLQZMSQcyTgMS6PvHwlIxaidHt6D8TZVd--JuwOraRnE6DpMMllPmIfdlxrrsSTz3NeAsIYKOt3PDItc3XK0MC9DlaimUQA5bEGYa9s9UBvBl1H~NPKGy2o4FbOXmNGs-1HMpkNZ3A-e4koyCGdQka8a22yxaVZ5cj7jYRiTMOPN2EQvmgGFFIZGdPliw__" alt="" />
+          <img className='absolute z-10' src="https://s3-alpha-sig.figma.com/img/c7c2/d1fe/2b00c87f8544a38ec58cc920e97751dd?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iLYRWm6xztHEMvijQz~RM0jrTF09QIiFOHhJ~lMnj2Gqs-5Cl7ANVEW0lMhBxmnFaGhg4M5y~EmVapzadWMSVhb7JARz5HlqTIN9DI5RWr20zXvcZTyL9~vIn9ZZZTs5SweNMMrN5bDjFfw~e3mDwW~CfEMoWdwENkbNdGJd1Yi~kpV~JKTShI04eUVdTj-eh1oJptLdtml-InLym-eql5lQ0QbxkBhYPXTbiML5OQMQoHEj4EFfQdL5n5uRnxDg2Df4WzdqXyraKqROjrXkCl-fNZFrhSAayfR~H2sDz0OueXkSP0PilAiWq5VLrwFqH1qtCqKKRe4WcKrBCDVViw__" alt="" />
+          <img className='absolute z-10' src="https://s3-alpha-sig.figma.com/img/5d5a/38aa/92597c1bd692a4f8656452f8f6dddf55?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AbMzECwQMyxniAkdHL7aK5SD3geF0tXxs915fl8B5M3f42FxJ7hnIZiuJycoggNFgmSpJ3d00-MOPgTunGUR7KU04kpWDqr2V8Apa~XAukgbSzgrDrEF~Rfs3NUlH~ABSBZ-pNsekHj0GeobiVAHcg3zCxG5f5J2pOt9AfR9BM8TXoAWaU0GLtcokMTgdkF5WEYLKloet30x3qTv9oRUuv39Y~IoOvzBV2EsOe~icKKDBYg9QxybJwP4XNvaACiafIuCAS909L1ZnunVw29u0dmc2br2EzrOAIvYxp1Kr29~5e1Kv70kEkNyx0Vfs1K3u2vt-CKoGcea8Cahtm5lyw__" alt="" />
+          <div>
+            <a href=""><button>ACIKTIM</button></a>
+            <span>
+              <p>KOD ACIKTIRIR Pizza, DOYURUR</p>
+              <p>fırsatı kaçırma</p>
+            </span>
+          </div>
+          </div>
+        </div>
+        
+      </>
     )
   }
 }
